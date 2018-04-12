b2c5014696da296d7902ed6b6ef734831aaf2260
Victor Ardulov <victor@ardulov.com>
2018-04-12 11:04:06 -0700

    implemented online DMDc with examples

--- online_dmd/control.py
+++ online_dmd/control.py
@@ -1,6 +1,8 @@
 import numpy as np
 import numpy.linalg as lin
 
+from .online import OnlineDMD
+
 class DMDc():
 
     def __init__(self, B=None):
@@ -58,3 +60,61 @@ class DMDc():
 
 
 
+class OnlineDMDc(OnlineDMD):
+
+    def __init__(self, B=None, tol=None, track=False):
+        super(OnlineDMDc, self).__init__(tol=tol, track=track)
+        self.known_controller = not(B is None)
+        self.B_k = B
+        self.p = 0
+        self.q = 0
+
+    def initial_fit(self, X, Y, Z):
+
+        self.p = X.shape[1]  # number of features in state
+        self.q = Y.shape[1]  # number of features in control
+        Delta = None
+        Omega = None
+
+        if self.known_controller:
+            Delta = (Z - np.matmul(Y, self.B_k))
+            Omega = X
+        else:
+            Delta = Z
+            Omega = np.hstack(X, Y)
+
+        super(OnlineDMDc, self).initial_fit(Omega, Delta)
+
+
+        if not(self.known_controller):
+            self.B_k = self.A_k[self.p:, :]
+            self.A_k = self.A_k[:self.p, :]
+            if self.B_k.shape[0] != self.q:
+                raise Exception('Something went wrong when computing control matrix!')
+
+    def update(self, X, Y, Z):
+        if self.iterations == 0:
+            self.initial_fit(X, Y, Z)
+        elif self.known_controller:
+            Delta = (Z - np.matmul(Y, self.B_k))
+            super(OnlineDMDc, self).update(X, Delta)
+        else:
+            for t in range(X.shape[0]):
+                if self.track:
+                    self.history.append((self.A_k, self.B_k))
+
+                x = X[t, :].reshape(1, -1)
+                y = Y[t, :].reshape(1, -1)
+                omega = np.hstack((x, y))
+                z = Z[t, :].reshape(1, -1)
+
+                self.gamma = (1 / (1 + np.matmul(omega, np.matmul(self.P_k, omega))))[0, 0]
+                G_k = np.vstack((self.A_k, self.B_k))
+                G_k += self.gamma * (np.matmul(self.P_k.T, np.matmul(omega.T, (z - np.matmul(omega, G_k)))))
+                self.P_k -= self.gamma * np.matmul(np.matmul(self.P_k, omega.T), np.matmul(omega, self.P_k))
+                self.A_k = G_k[:self.p, :]
+                self.B_k = G_k[self.p:, :]
+                if self.B.shape[0] != self.q:
+                    raise Exception('Something went wrong when computing control matrix!')
+
+                self.iterations += 1
